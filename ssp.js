6fb8536628940ce79695ce838ffc9e73724c384f
Rob <robert.chamberlain@cmeducations.se>
2023-12-01 15:23:37 +0100

    Initialising repo

---
+++ ssp.js
@@ -0,0 +1,30 @@
+const choices = ["sten", "sax", "påse"]
+let winner;
+
+while (winner === undefined) {
+    let computerChoice = choices[Math.floor(Math.random() * choices.length)];
+
+    let userChoice;
+    let pattern = /^(sten|sax|påse){1}$/i;
+    
+    while(!pattern.test(userChoice)) {
+        userChoice =prompt("Choose sten, sax or påse");
+    }
+
+    if (userChoice !== computerChoice) {
+        if (userChoice === "sten") {
+            winner = computerChoice === "sax" ? "User" : "Computer";
+        }
+        else if (userChoice === "sax") {
+            winner = computerChoice === "påse" ? "User" : "Computer";
+        }
+        else {
+            winner = computerChoice === "sten" ? "User" : "Computer";
+        }
+    } else {
+        alert("Its a draw!!")
+    }
+}
+
+alert(`${winner} has won!!`)
+
